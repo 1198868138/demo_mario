9cc2e319cade895ebb0b4ba5a57c1a5673fdab4d
Vincent Zhong <suppernoname@163.com>
2024-07-11 15:30:23 +0800

    add feature #1

--- mario.py
+++ mario.py
@@ -19,17 +19,47 @@ def block():
         print()
 
 
+# triangle
+def tag(n):
+    # 每行的操作
+    for row in range(n):
+        # 计算需要的井号和空格的个数
+        hashes = row + 1
+        dots = n - hashes
+        # 打印点
+        for dot in range(dots):
+            print(".", end="")
+        # 打印井号
+        for hashe in range(hashes):
+            print("#", end="")
+        # 打印换行符
+        print()
+
+
+# double triangle        
+def dtag(n):
+    # 每行的操作
+    for row in range(n):
+        # 计算需要的井号和空格的个数
+        hashes = row + 1
+        dots = n - hashes
+        # 打印点
+        for dot in range(dots):
+            print(".", end="")
+        # 打印井号
+        for hashe in range(hashes):
+            print("#", end="")
+        # 打印中间点
+        print("..", end="")
+        # 打印井号
+        for hashe in range(hashes):
+            print("#", end="")
+        # 打印点
+        for dot in range(dots):
+            print(".", end="")
+        # 打印换行符
+        print()
+
+
 n = int(input("height: "))
-# 每行的操作
-for row in range(n):
-    # 计算需要的井号和空格的个数
-    hashes = row + 1
-    dots = n - hashes
-    # 打印点
-    for dot in range(dots):
-        print(".", end="")
-    # 打印井号
-    for hashe in range(hashes):
-        print("#", end="")
-    # 打印换行符
-    print()
+dtag(n)
